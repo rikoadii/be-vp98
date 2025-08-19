1481aabe91f83f3d777b015f1b90444a1152ccf8
ADAM <2201020027@universitasbumigora.ac.id>
2025-08-19 20:00:10 +0800

    Revert database host configuration to localhost for local development

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 // Konfigurasi database
-$host = 'https://admin.victoryproduction98.com';
+$host = 'localhost'; // atau '127.0.0.1'
 $dbname = 'u967345369_db_98';
 $username = 'u967345369_victoryproduct';
 $password = '.k8cB$@AnBPHBZU';
@@ -11,15 +11,16 @@ $conn = mysqli_connect($host, $username, $password, $dbname);
 // Cek koneksi
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi database berhasil!";
 }
 
 // Set charset
 mysqli_set_charset($conn, "utf8");
-
-// echo "Koneksi database berhasil!";
 
 // Fungsi untuk mendapatkan koneksi database
 function getConnection() {
     global $conn;
     return $conn;
 }
+?>
